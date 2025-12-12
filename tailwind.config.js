e0e39a38245331dd1f7fc16529d060e0e6d95689
jayaraj <madhesjayaraj@gmail.com>
2025-12-12 11:24:46 +0530

    navbar chnaged

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,21 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        "pulse-slow": {
+          '0%, 100%': {
+            transform: 'translateX(-100%)',
+          },
+          '50%': {
+            transform: 'translateX(100%)',
+          },
+        },
+      },
+      animation: {
+        "pulse-slow": "pulse-slow 3s ease-in-out infinite",
+      },
+    },
   },
   plugins: [],
 }
